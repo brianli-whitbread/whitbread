f984a08dfda84e997e5a695c820bf0f97bd79329
Brian Li <brianli@ensemble.com>
2018-08-02 21:55:28 +0100

    whitbread-11 : carousel and test carousel works fine

--- core/src/test/java/uk/co/whitbread/assignment/core/wcm/CarouselTest.java
+++ core/src/test/java/uk/co/whitbread/assignment/core/wcm/CarouselTest.java
@@ -1,18 +1,26 @@
 package uk.co.whitbread.assignment.core.wcm;
 
+import com.adobe.cq.commerce.common.ValueMapDecorator;
+import com.adobe.cq.sightly.WCMBindings;
+import com.google.common.collect.ImmutableMap;
 import org.apache.commons.io.IOUtils;
+import org.apache.sling.api.resource.ValueMap;
 import org.apache.sling.commons.json.JSONArray;
 import org.apache.sling.commons.json.JSONException;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import javax.script.Bindings;
+import javax.script.SimpleBindings;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringWriter;
+import java.util.HashMap;
 
+import static org.mockito.Mockito.doCallRealMethod;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -22,39 +30,39 @@ public class CarouselTest {
     private final String LOCAL_RESOURCE_CAROUSEL_ITEMS = "carouselItems.json";
     private final String WCMUSEPOJO_PROP_NAME_CAROUSEL_ITEMS_JSON = "carouselItemsJSON";
 
+    private Carousel carousel = new Carousel();
+    private SimpleBindings simpleBindings = new SimpleBindings();
+
     @Test
     public void testPassIfCarouselIDIsNotEmpty() {
-        Bindings bindings = mock(Bindings.class);
-        Carousel carousel = new Carousel();
-        carousel.init(bindings);
+        carousel.init(simpleBindings);
         Assert.assertTrue(!carousel.getCarouselID().isEmpty());
     }
 
     @Test
     public void testPassIfWrongJSONFormatReturnsArrayListWith0Items() throws Exception {
-        Bindings bindings = mock(Bindings.class);
         String[] carouselItemsJSON = {"_"};
-        when(bindings.get(WCMUSEPOJO_PROP_NAME_CAROUSEL_ITEMS_JSON)).thenReturn(carouselItemsJSON);
-        Carousel carousel = new Carousel();
-        carousel.init(bindings);
-        Assert.assertTrue(carousel.getCarouselItems().size() == 0);
+        ValueMap properties = new ValueMapDecorator(ImmutableMap.<String, Object> of(WCMUSEPOJO_PROP_NAME_CAROUSEL_ITEMS_JSON, carouselItemsJSON));
+        simpleBindings.put(WCMBindings.PROPERTIES, properties);
+        carousel.init(simpleBindings);
+        Assert.assertTrue( carousel.getCarouselItems().size() == 0);
     }
 
     @Test
     public void testPassIfJsonStringReturnsArrayWith3Items() throws IOException, JSONException {
-        Bindings bindings = mock(Bindings.class);
         String[] carouselItemsJSON = getCarouselItemsJsonAsStringArray();
-        when(bindings.get(WCMUSEPOJO_PROP_NAME_CAROUSEL_ITEMS_JSON)).thenReturn(carouselItemsJSON);
-        Carousel carousel = new Carousel();
-        carousel.init(bindings);
-        Assert.assertTrue(carousel.getCarouselItems().size() == 3);
+        ValueMap properties = new ValueMapDecorator(ImmutableMap.<String, Object> of(WCMUSEPOJO_PROP_NAME_CAROUSEL_ITEMS_JSON, carouselItemsJSON));
+        simpleBindings.put(WCMBindings.PROPERTIES, properties);
+        carousel.init(simpleBindings);
+        Assert.assertTrue( carousel.getCarouselItems().size() == 3);
     }
 
     @Test
     public void testPassIfWrongValidPropertyReturnsArrayListWith0Items() throws Exception {
-        Bindings bindings = mock(Bindings.class);
-        Carousel carousel = new Carousel();
-        carousel.init(bindings);
+        String[] carouselItemsJSON = getCarouselItemsJsonAsStringArray();
+        ValueMap properties = new ValueMapDecorator(ImmutableMap.<String, Object> of("carouselItemsJSON#", carouselItemsJSON));
+        simpleBindings.put(WCMBindings.PROPERTIES, properties);
+        carousel.init(simpleBindings);
         Assert.assertTrue(carousel.getCarouselItems().size() == 0);
     }
 
